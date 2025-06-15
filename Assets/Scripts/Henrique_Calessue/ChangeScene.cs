36367e9b397c0568ede16a4aeff628afc17f7ccd
Henrique2003up <calessuehenrique2023@gmail.com>
2025-06-15 23:55:20 +0100

    Final só falta o video

---
+++ Assets/Scripts/Henrique_Calessue/ChangeScene.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName;
+    public void ChangeSceneAction()
+
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void UnloadLoseScene()
+    {
+        SceneManager.UnloadSceneAsync("LoseScene");
+    }
+}
+
+
