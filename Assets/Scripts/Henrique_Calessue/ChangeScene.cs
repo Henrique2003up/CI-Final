b97526978e78cc8a3d4abe71cfe4d49c0cd5c489
Henrique2003up <calessuehenrique2023@gmail.com>
2025-06-15 01:06:40 +0100

    scene

--- Assets/Scripts/Henrique_Calessue/ChangeScene.cs
+++ Assets/Scripts/Henrique_Calessue/ChangeScene.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class ChangeScene : MonoBehaviour
 {
-    public string SceneName;
-
+    public string sceneName;
     public void ChangeSceneAction()
     {
-        SceneManager.LoadScene(SceneName);
+        SceneManager.LoadScene(sceneName);
     }
     public void UnloadLoseScene()
     {
